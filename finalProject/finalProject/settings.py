ed78f4310cfacd08bf872580a66fc2e69dd36cd7
OussamaBendriss <oussama.b1@live.fr>
2021-04-14 16:37:37 +0000

    big design changes

--- finalProject/finalProject/settings.py
+++ finalProject/finalProject/settings.py
@@ -119,11 +119,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/logos/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
